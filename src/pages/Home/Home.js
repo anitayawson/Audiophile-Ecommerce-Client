47606fc5f1fad453a466978ccb2cf3c2ffd15ba2
Anita Yawson <anitayawson@gmail.com>
2024-03-20 00:35:38 -0400

    fetch new product,categories and render on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,67 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./Home.scss";
 import NavBar from "../../components/NavBar/NavBar";
 
+// const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 export default function Home() {
-  const baseUrl = process.env.REACT_APP_API_BASE_URL;
+  const [newProduct, setNewProduct] = useState({});
+  const [categories, setCategories] = useState([]);
+
+  const fetchNewProduct = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/products/category/1?isNew=1"
+      );
+      setNewProduct(response.data[0]);
+      // console.log(response.data[0]);
+    } catch (error) {
+      console.error("Error fetching new product:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNewProduct();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/categories");
+      setCategories(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   return (
-    <header className="header">
-      <NavBar />
-      <div className="header__body">
-        <p className="header__overline">New Product</p>
-        <h2 className="header__title">XX99 Mark II HeadphoneS</h2>
-        <p className="header__description">
-          Experience natural, lifelike audio and exceptional build quality made
-          for the passionate music enthusiast.
-        </p>
-        <button className="header__btn">See Product</button>
-      </div>
-    </header>
+    <main>
+      <header className="header">
+        <NavBar />
+        <div className="header__body">
+          <p className="header__overline">New Product</p>
+          <h2 className="header__title">{newProduct.name}</h2>
+          <p className="header__description">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <button className="header__btn">See Product</button>
+        </div>
+      </header>
+      <section>
+        <section className="category-list">
+          {categories.map((category) => (
+            <div key={category.id}>
+              <p>{category.name}</p>
+            </div>
+          ))}
+        </section>
+      </section>
+    </main>
   );
 }
